3cff722f264d4eb56695e2fc0816c33c55368d8b
Mukesh Suman <sumanm686@gmail.com>
2020-04-10 07:41:20 +0530

    some changes in picklist

--- picklists/picklist.model.js
+++ picklists/picklist.model.js
@@ -7,8 +7,12 @@ const schema = new Schema(
     value: { type: String, required: true },
     type: { type: String, required: true },
     parentName: { type: String, required: false },
+    parentValue: { type: String, required: false },
     rate: { type: Number, required: true,  default: 0 },
+    labourCharge: { type: Number, required: true,  default: 0 },
     isActive: { type: Boolean, required: true, default: true },
+    isAvailSubCat: { type: Boolean, required: true, default: false },
+    defaultActive: { type: Boolean, required: true, default: false },
     createdBy: { type: Schema.Types.ObjectId, required: true, ref: "User" }
   },
   {
